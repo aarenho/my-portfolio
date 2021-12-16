ac3fb0405defafdcf8c3c5ced0218e30bb00b674
Aaren Ho <aarenho11@gmail.com>
2021-12-16 02:06:42 -0800

    2000 random stars added

--- 3d-portfolio/main.js
+++ 3d-portfolio/main.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene();
 
@@ -9,26 +10,52 @@ const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
 
- renderer.setPixelRatio(window.devicePixelRatio);
- renderer.setSize(window.innerWidth, window.innerHeight);
- camera.position.setZ(30);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio)
+camera.position.setZ(30);
 
- renderer.render( scene, camera );
+// renderer.render( scene, camera );
 
- const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
- const material = new THREE.MeshBasicMaterial( { color: 0xFF6347, wireframe: true } );
- const torus = new THREE.Mesh( geometry, material );
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshStandardMaterial( { color: 0xFF6347  } );
+const torus = new THREE.Mesh( geometry, material );
 
- scene.add(torus);
+scene.add(torus);
 
- function animate() {
-   requestAnimationFrame( animate )
+// Point Light
+const pointLight = new THREE.PointLight(0xFFFFFF)
+pointLight.position.set(5, 5, 5) 
+// Ambient Light
+const ambientLight = new THREE.AmbientLight(0xFFFFFF)
+// Helpers
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200, 50)
 
-   torus.rotation.x += 0.01
-   torus.rotation.y += 0.005
-   torus.rotation.z += 0.01
+scene.add(pointLight, ambientLight, lightHelper, gridHelper )
 
-   renderer.render( scene, camera );
- }
+const controls = new OrbitControls(camera, renderer.domElement)
 
- animate()
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24)
+  const material = new THREE.MeshStandardMaterial({color: 0xFFFFFF})
+  const star = new THREE.Mesh(geometry, material)
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100))
+  star.position.set(x, y, z)
+  scene.add(star)
+}
+
+Array(2000).fill().forEach(addStar)
+
+function animate() {
+  requestAnimationFrame( animate )
+
+  torus.rotation.x += 0.01
+  torus.rotation.y += 0.005
+  torus.rotation.z += 0.01
+
+  // controls.update()
+  renderer.render( scene, camera );
+}
+
+animate()
