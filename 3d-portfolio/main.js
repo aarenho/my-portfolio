16e85bb89acb4d67ea24c78d08aa7ec1ff8fd742
Aaren Ho <aarenho11@gmail.com>
2021-12-16 01:44:20 -0800

    spinning torus

--- 3d-portfolio/main.js
+++ 3d-portfolio/main.js
@@ -23,6 +23,11 @@ const renderer = new THREE.WebGLRenderer({
 
  function animate() {
    requestAnimationFrame( animate )
+
+   torus.rotation.x += 0.01
+   torus.rotation.y += 0.005
+   torus.rotation.z += 0.01
+
    renderer.render( scene, camera );
  }
 
